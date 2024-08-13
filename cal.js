8187468a88b4e1da4ffe34febff6d3eb512d709b
evSnow <evanrodriguez2022@gmail.com>
2024-08-13 01:11:47 -0500

    add the ui, opration, and fixes some operation bugs

--- cal.js
+++ cal.js
@@ -1,9 +1,10 @@
 let current=[];
 let sumCurrent=0;
 let sum=0;
-let symbol=0;
+let symbol="_";
+let symbolPre="_";
 const num='0123456789';
-let userChoice
+const displayCurrent= document.querySelector("#displayCurrent");
 
 function add(a,b){
     return a+b;
@@ -15,8 +16,10 @@ function multiply(a,b){
     return a*b;
 }
 function divide(a,b){
-    if(b==0){
-        return false;
+    if(b===0){
+        console.log("hi");
+        display("n/a");
+        return "n/a";
     }
     return a/b;
 }
@@ -34,13 +37,14 @@ function operate(a,b,option){
         return multiply(a,b);
     }
     else if (option==="/"){
+        console.log((a + "/" + b));
         return divide(a,b);
     }
     else if (option==="="){
-        return divide(a,b);
+        return;
     }
     else{
-        return "error";
+        return "n/a";
     }
 }
 function compile(current){
@@ -50,33 +54,83 @@ function compile(current){
     }
     return string;
 }
-function display(current){
-    console.log(current.length);
-    for(let i=0; i<current.length; i++){
-        console.log(current[i]);
-        
-    }
+function display(show){
+    const content=document.getElementById("dis");
+    content.textContent=show;
+    displayCurrent.appendChild(content);
 }
 const buttons=document.querySelectorAll("button");
 buttons.forEach((button)=>{
     button.addEventListener("click",() =>{
-
-        if(isNaN(button.id)){
+        if(button.id==="clear"){
+            current=[];
+            sumCurrent=0;
+            sum=0;
+            symbol="_";
+            display(sum);
+        }
+        else if(sum==="n/a"){
+            return display("n/a (plese clear)");
+        }
+        else if(isNaN(button.id)){
             if(button.id==="="){ 
-            console.log(sumCurrent);
-            sum=operate(sum,sumCurrent,symbol);
-            console.log(sum);
+
+                 if(symbolPre!=="_"){
+                    console.log(sumCurrent);
+                    sum=operate(sum,sumCurrent,symbolPre);
+                        if(sum==="n/a"){
+                            return;
+                        }
+                    console.log(sum);
+                    sumCurrent=0;
+                    display(sum);
+                    symbol="_"
+                }
+                else{
+                     console.log(sumCurrent);
+                     sum=operate(sum,sumCurrent,symbol);
+                     console.log(sum);
+                     if(sum==="n/a"){
+                        return;
+                    }
+                     display(sum);
+                     symbolPre=symbol;
+                     sumCurrent=0;
+                     symbol="_"
+                     
+                }
+            }
+            else if(symbol!=="_"){
+                console.log(sumCurrent);
+                sum=operate(sum,sumCurrent,symbol);
+                sumCurrent=0;
+                console.log(sum);
+                display(sum);
+                symbol=button.id;
+                current=[];
             }
             else{
-                symbol=button.id
-                sum=sumCurrent;
-                current=[];
+                if(symbol==="_"){ 
+                    symbol=button.id;
+                    if(sumCurrent!==0){ 
+                    sum=sumCurrent;
+                    }
+                    current=[];
+                    display(sum);
+                    symbolPre="_";
+                }
+                else{
+                    symbol=button.id;
+                    symbolPre="_";
+                    current=[];
+                }
             }
         }
         else{
         current.push(button.id)
         sumCurrent=compile(current);
         sumCurrent=parseInt(sumCurrent);
+        display(sumCurrent);
         }
        
         //display(current);
